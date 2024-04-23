df3c5ecb811ed39a5641b1a20d351e536ae2a89d
webcat12345 <rango0986@gmail.com>
2024-04-23 05:16:07 +0200

    chains: add sanko

--- src/buildList.js
+++ src/buildList.js
@@ -2,12 +2,13 @@ const { version } = require("../package.json");
 require('dotenv').config()
 const arbitrumSepolia = require("./tokens/arbitrum-sepolia.json");
 const arbitrumOne = require("./tokens/arbitrum-one.json");
+const sanko = require("./tokens/sanko.json");
 const xai = require("./tokens/xai.json");
 
 const BASE_URL = process.env.BASE_URL || "https://token-list.camelot.exchange"
 
 module.exports = function buildList() {
-    const tokens = [...arbitrumSepolia, ...arbitrumOne, ...xai]
+    const tokens = [...arbitrumSepolia, ...arbitrumOne, ...sanko, ...xai]
   const processedTokens = JSON.parse(JSON.stringify(tokens).replace(/BASE_URL/g, BASE_URL))
   const parsed = version.split(".");
 
