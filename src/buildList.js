e030392f576506950fc905c646d6e8e4c21ce819
webcat12345 <rango0986@gmail.com>
2024-06-06 13:47:23 +0200

    chains: add Rari

--- src/buildList.js
+++ src/buildList.js
@@ -2,13 +2,14 @@ const { version } = require("../package.json");
 require('dotenv').config()
 const arbitrumSepolia = require("./tokens/arbitrum-sepolia.json");
 const arbitrumOne = require("./tokens/arbitrum-one.json");
+const rari = require("./tokens/rari.json");
 const sanko = require("./tokens/sanko.json");
 const xai = require("./tokens/xai.json");
 
 const BASE_URL = process.env.BASE_URL || "https://token-list.camelot.exchange"
 
 module.exports = function buildList() {
-    const tokens = [...arbitrumSepolia, ...arbitrumOne, ...sanko, ...xai]
+    const tokens = [...arbitrumSepolia, ...arbitrumOne, ...rari, ...sanko, ...xai]
   const processedTokens = JSON.parse(JSON.stringify(tokens).replace(/BASE_URL/g, BASE_URL))
   const parsed = version.split(".");
 
